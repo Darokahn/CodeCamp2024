9ddbd50406031d994161774f5246308db3f773c9
Andrew <eardley.andrew1@gmail.com>
2024-11-15 14:41:45 -0500

    tweaked movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,37 @@
-extends Node2D
+extends RigidBody2D
 
-var body: CharacterBody2D
-var hammer: CollisionObject2D
-var onGround: bool
-var respectsGravity: bool
+var reduced_gravity = 1
 
+var default_gravity = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	body = $CharacterBody2D
-	hammer = $Area2D
-	onGround = false
-	respectsGravity = true
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var bodies: Array[Node2D] = $onGround.get_overlapping_bodies()
+	var on_ground = bodies.size() > 1
+	if Input.is_action_just_pressed("move_up"):
+		if on_ground:
+			apply_central_impulse(Vector2(0, -1500))
+	if Input.is_action_pressed("move_up"):
+		gravity_scale = reduced_gravity
+		
+	else:
+		gravity_scale = default_gravity
+	if Input.is_action_pressed("move_left"):
+		if on_ground:
+			apply_central_impulse(Vector2(-70, 0))
+		apply_central_impulse(Vector2(-20, 0))
+	if Input.is_action_pressed("move_right"):
+		if on_ground:
+			apply_central_impulse(Vector2(70, 0))
+		apply_central_impulse(Vector2(20, 0))
+	
+func _physics_process(delta: float) -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_right"):
-		body.velocity.x += 200 * delta
-	if Input.is_action_pressed("move_left"):
-		body.velocity.x -= 200 * delta
-	if Input.is_action_just_pressed("move_up"):
-		body.velocity.y -= 20000 * delta
-	if Input.is_action_pressed("move_down"):
-		body.velocity.y += 200 * delta
-	
-	body.position += body.velocity * delta
-	
-	body.velocity *= Globals.AIR_FRICTION
-	if onGround:
-		body.velocity *= Globals.GROUND_FRICTION
-	if respectsGravity:
-		body.velocity.y += Globals.GRAVITY * delta
-	
-		
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	pass
