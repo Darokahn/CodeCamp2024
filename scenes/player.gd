8a854d9bd4083b8e5e222892440bed933a774cea
Andrew <eardley.andrew1@gmail.com>
2024-11-15 13:53:02 -0500

    jumping work better even better

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,12 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var bodies: Array[Node2D] = $onGround.get_overlapping_bodies()
+	if Input.is_action_just_pressed("move_up") and bodies.size() > 1:
+		apply_central_impulse(Vector2(0, -200))
 	
 func _physics_process(delta: float) -> void:
 	pass
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if Input.is_action_just_pressed("move_up") and $onGround.get_overlapping_bodies().size() > 1:
-		apply_central_impulse(Vector2(0, -200))
-		print("jumped")
+	pass
